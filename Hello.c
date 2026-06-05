6f5e528136d71359a72648ea760a1e14f28f4819
Siddharth Bhardwaj <2008siddharthbhardwaj@gmail.com>
2026-06-05 16:40:32 +0530

    My first c program

---
+++ Hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello,World I am Siddharth and this is my first C program . Its going to be a journey because i am new to programming and after alot of confusion and havingnot many options i chose to do bca there is still 2 months before i go to college , i plan on going to VIPS and i scored pretty good somehow , i ranked 183!. Crazy ik but lets hope it all goes well , when i tell someone i am doing bca they all make a face so lol. \n");
+}
